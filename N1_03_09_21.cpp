723f7cb80ee245f2226258e2438647eee988f555
Gulzad <48948493+awesic@users.noreply.github.com>
2021-09-03 22:13:04 +0300

    Add files via upload

---
+++ N1_03_09_21.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int quant;
+    float price;
+    float sum = 0;
+    cout << "Введите количество товаров: ";
+    cin >> quant;
+    for (int i = 0; i < quant; i++) {
+        cout << "Цена " << i + 1 << " товара: ";
+        cin >> price;
+        sum += price;
+    }
+    cout << "Итого: " << sum << endl;
+}
+
