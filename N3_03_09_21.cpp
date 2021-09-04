0ca8374da0a25abd33889c19f22db8b8662c519f
Gulzad <48948493+awesic@users.noreply.github.com>
2021-09-04 10:13:15 +0300

    Add files via upload

---
+++ N3_03_09_21.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1234;
+    cout << a % 10;
+    a /= 10;
+    cout << a % 10;
+    a /= 10;
+    cout << a % 10;
+    a /= 10;
+    cout << a % 10 << endl;
+
+    return 0;
+}
