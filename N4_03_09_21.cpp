79794b61d13a3ff3257939017de2fa81d0ee47aa
Gulzad <48948493+awesic@users.noreply.github.com>
2021-09-04 10:33:39 +0300

    Add files via upload

---
+++ N4_03_09_21.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x;
+    char exit = '0';    
+    while (exit != 'x')
+    {
+        cout << "Введите число для возведения в куб: ";
+        cin >> x;
+        cout << pow(x, 3) << endl;
+        cout << "Для выхода нажмите 'x' или букву для продолжения ";
+        cin >> exit;
+    }
+
+    return 0;
+}
