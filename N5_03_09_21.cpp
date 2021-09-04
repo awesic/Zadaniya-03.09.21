14caaafcfb731d29e76ddaea5eba860a30d2f04a
Gulzad <48948493+awesic@users.noreply.github.com>
2021-09-04 10:48:53 +0300

    Add files via upload

---
+++ N5_03_09_21.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0, y = 0, sum = 0;
+    int i = x;
+    cout << "Введите диапазон чисел через enter: ";
+    cin >> x;
+    cin >> y;
+    while (i <= y)
+    {
+        sum += i;
+        i++;
+    }
+    cout << "\nСумма в диапазоне от " << x << " до " << y << " = " << sum << endl;
+
+    return 0;
+}
